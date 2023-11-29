67d683e4193aaf6ee213a8e63935996da401c564
wendel guilherme <wendel.guilherme@estudante.ufjf.br>
2023-11-28 22:55:09 -0300

    funcao getestoque

---
+++ estoque.js
@@ -0,0 +1,14 @@
+let estoque = {
+  joao: {
+    maca: 1,
+    banana: 2,
+  },
+  maria: {
+    maca: 2,
+    pera: 3,
+  },
+};
+
+function getEstoque() {
+  return estoque;
+}
--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+const olJoao=document.querySelector("#joao");
+const olMaria=document.querySelector("#maria");
+
 document.entrada.addEventListener('submit', leFormulario);
 
-function leFormulario(params){
+function leFormulario(event);
     event.preventDefault();
-    const quantidade = document.entrada.quantidade.value;
+    const quantidade = document.entrada.quantidade.valueAsNumber;
     const fruta = document.entrada.fruta.value;
-    console.log(`Eu tenho ${quantidade} ${fruta}`);
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+
+
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 }
