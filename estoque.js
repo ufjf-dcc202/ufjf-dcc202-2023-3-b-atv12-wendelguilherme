cf3ec893929830a1dcdd6a8f6ab9f9aca3a3f863
a <a>
2023-12-05 22:53:50 -0300

    add functions origem p destino

--- estoque.js
+++ estoque.js
@@ -1,16 +1,83 @@
 let estoque = {
-  joao: {
-    maca: 1,
-    banana: 2,
-  },
-  maria: {
-    maca: 2,
-    pera: 3,
-  },
+  'joao': [
+    {'tipo': 'maca', 'qtd':1},
+    {'tipo': 'pera', 'qtd':2}
+
+  ],
+  'maria': [
+    {'tipo': 'maca', 'qtd':1},
+    {'tipo': 'banana', 'qtd':4}
+
+  ],
 };
 
 function getEstoque() {
   return structuredClone(estoque);
 }
 
-export { getEstoque };
+function transacao(origem, destino, tipo, quantidade){
+  if (origem === destino){ return; }
+
+  if (destino==="pomar"){
+    dePessoaParaPomar(origem,tipo,destino);
+    return;
+  }
+  if (origem==="pomar"){
+    dePomarParaPessoa(destino,tipo, quantidade);
+    return;
+  }
+
+  const pessoaOrigem= estoque[origem];
+  const pessoaDestino = estoque[destino];
+  let monteOrigem;
+  for(let i=0; i<pessoaOrigem.lenght;i++){
+    const monte = pessoaOrigem[i];
+    if ((monte.tipo)=== tipo){
+      monteOrigem=monte;
+      break;
+    }
+  }
+  if (!monteOrigem) {return; }
+  let monteDestino;
+  for(let i=0; i<pessoaDestino.lenght; i++){
+    const monte = pessoaDestino[i];
+    if (monte.tipo===tipo){
+      monteDestino = monte;
+      break;
+    }
+  }
+  if (!monteDestino){
+    monteDestino= {'tipo': tipo, qtd: 0};
+    pessoaDestino.push(monteDestino);
+  }
+  const qtdReal =Math.min(quantidade, monteOrigem.qtd);
+  monteDestino.qtd += qtdReal;
+  monteOrigem.qtd -= qtdReal;
+}
+
+function dePomarParaPessoa(destino,tipo, quantidade){
+  const pessoa= estoque[destino];
+  for(let i=0; i<pessoa.lenght;i++){
+    const monte=pessoa[i];
+    if (monte.tipo===tipo){
+      monte.qtd+=Math.max(quantidade, 0);
+      return;
+    }
+  }
+  const novoMonte= {'tipo':tipo, 'qtd': Math.max(quantidade, 0)};
+  pessoa.push(novoMonte)
+
+}
+
+function dePessoaParaPomar(origem, tipo, quantidade){
+  const pessoa= estoque[origem];
+  for(let i=0; i<pessoa.lenght;i++){
+    const monte=pessoa[i];
+    if (monte.tipo===tipo){
+      monte.qtd-=Math.min(quantidade, monte.qtd);
+    }
+  }
+}
+
+
+export {getEstoque, transacao};
--- main.js
+++ main.js
@@ -1,15 +1,37 @@
+import { getEstoque } from "./estoque.js";
+
 const olJoao=document.querySelector("#joao");
 const olMaria=document.querySelector("#maria");
 
 document.entrada.addEventListener('submit', leFormulario);
 
-function leFormulario(event);
+atualizaTela();
+
+function leFormulario(event){
     event.preventDefault();
     const quantidade = document.entrada.quantidade.valueAsNumber;
     const fruta = document.entrada.fruta.value;
     const origem = document.entrada.origem.value;
     const destino = document.entrada.destino.value;
 
-
     console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+
+    transacao(origem, destino, fruta, quantidade);
+    atualizaTela();
+}
+
+function atualizaTela(){
+    const estoque= getEstoque();
+    preencheLista(olJoao, estoque.joao);
+    preencheLista(olMaria, estoque.maria);
+}
+
+function preencheLista(lista, estoqueDaPessoa){
+    lista.innerHTML = "";
+    for (let i=0; i<estoqueDaPessoa.length; i++){
+        const monte=estoqueDaPessoa[i];
+        const li = document.createElement('li');
+        li.textContent= `${monte.tipo}: ${monte.qtd}`;
+        lista.append(li);
+    }
 }
